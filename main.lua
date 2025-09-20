7ec3be982bbfcc3ebccd54bcacab18380d37675f
Roméo Poirier <romeo.poirier@epitech.eu>
2025-09-20 19:33:37 +0200

    new (main) fps limited render à la mano parce qu'on est trop bg

--- main.lua
+++ main.lua
@@ -5,6 +5,11 @@ _G.love = require("love")
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
+TARGET_FPS = 60
+DRAW_INTERAL = 1000 / TARGET_FPS
+last_loop_time = love.timer.getTime() * 1000
+AVG_RUN_TIME = 0
+
 require "block"
 require "player"
 require "camera"
@@ -32,8 +37,7 @@ function love.load()
     b2 = Block:new(1050, 50, 700, 500, 4)
 end
 
-
-function love.update(dt)
+function run_game(dt)
     player.handle_inputs(player, camera)
     player.apply_gravity(player)
     Player.apply_movements(player, camera)
@@ -45,6 +49,24 @@ function love.update(dt)
     block_list_info()
     print()
     update_calls = update_calls + 1
+end
+
+function fps_limited_loop()
+    local current_time = love.timer.getTime() * 1000 -- *1000 to transform into ms
+    local delta_time = current_time - last_loop_time
+
+    print("current time: "..current_time.."; last loop time: "..last_loop_time)
+    print("average runtime: "..AVG_RUN_TIME.."; draw interval: "..DRAW_INTERAL)
+    if (current_time - last_loop_time >= DRAW_INTERAL - AVG_RUN_TIME) then
+        last_loop_time = love.timer.getTime() * 1000 -- restart clock
+        run_game(delta_time)
+        AVG_RUN_TIME = (AVG_RUN_TIME * 0.7 +
+        (love.timer.getTime() * 1000 - last_loop_time) * 1.3) / 2.0
+    end
+end
+
+function love.update(dt)
+    fps_limited_loop(dt)
     --print("\27[4;31mUpdate call: "..update_calls..", current fps:"..love.timer.getFPS().."\27[0;0m")
     print("\n\n")
 end
